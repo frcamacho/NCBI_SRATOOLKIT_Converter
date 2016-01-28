3d4796bd0dd7c0a77240876bc673765dc82fae19
Francine Camacho <camachofrancine@gmail.com>
2016-01-28 16:35:53 -0500

    updated SRA_fastq.py to include terminal arguments

--- SRA_fastq.py
+++ SRA_fastq.py
@@ -1,43 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thursday Jan 28 12:01:12 2016
 
-#!/usr/bin/python
-
+@author: francinecamacho
+"""
+from multiprocessing import Process 
 import sys 
 import os
 import re
 import subprocess
 
-#Script to convert NCBI SRA files to FASTQ files using NCBI SRA toolkit 
-
-def converter(SRR_list, path): 
+def sra_To_Fastq(sra_dir_path, outdir_path): 
 	
-	i = 0 
+# changes path to dump the fastq in the fastq directory 
 
-	length = len(SRR_list)
-
-	os.chdir(path) # changes path to dump the fastq in the fastq directory 
-
-	for i in range(0,length): 
+	for sraFile in os.listdir(sra_dir_path):
 		
-		cmd = "fastq-dump -I --split-files --outdir . " + SRR_list[i] # converts SRA to fastq and splits up the forward and reverse reads in different files   
+		newprocess = Process(target=convertSRA_to_fastq,args=(sraFile,sra_dir_path,outdir_path,))
+		newprocess.start()
+		newprocess.join()
 
-		subprocess.call(cmd, shell=True) # terminal command 
+def convertSRA_to_fastq(sraFile, sra_dir_path, outdir_path):
+	sraName = sraFile.split(".")[0]
+		
+	cmd = "fastq-dump -A "+ sraName + " " + sra_dir_path +"/"+ sraFile + " -I --split-files --outdir " + outdir_path # converts SRA to fastq and splits up the forward and reverse reads in different files   
+	print cmd 
+	subprocess.call(cmd, shell=True) # terminal command 
 
-		i += 1
-
+def main(sra_dir_path, sra_dir_folder, outdir_path, outdir_folder): 
+	os.chdir(sra_dir_path)
+	sra_dir_path = os.path.join(sra_dir_path, sra_dir_folder)
+	outdir_path = os.path.join(outdir_path, outdir_folder)
+	os.mkdir(outdir_path)
+	sra_To_Fastq(sra_dir_path, outdir_path)
 
 if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--sra_dir_path', type= str, required=True)
+	parser.add_argument('--sra_dir_folder', type= str, required=True)
+	parser.add_argument('--outdir_path', type=str, required=True)
+	parser.add_argument('--outdir_folder', type=str, required=True)
 
-	SRR_FileNname = sys.argv[1]
-	print SRR_FileNname
+	args = parser.parse_args()
 
-	with open(SRR_FileNname, 'r') as filename: # 
-	
-		SRR_Acc_List = [line.strip() for line in filename] # reads each line and saves it in a list 
-
-	filename.close()
-
-	file_pwd = sys.argv[2]
-	print file_pwd
-
-	converter(SRR_Acc_List, file_pwd)
-
+	main(args.sra_dir_path, args.sra_dir_folder, args.outdir_path, args.outdir_folder)
